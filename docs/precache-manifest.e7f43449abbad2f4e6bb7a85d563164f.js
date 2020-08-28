d6201004609ec10fca61619362faf2d22b2c049a
luise <luise@DESKTOP-CDLK835>
2020-08-28 05:14:59 -0500

    docs agregado

---
+++ docs/precache-manifest.e7f43449abbad2f4e6bb7a85d563164f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f778cce7ff77d1dd1734b1a85a410e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bc0f5eb6eb19fd0439a",
+    "url": "/static/css/main.5e8de72c.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc0f5eb6eb19fd0439a",
+    "url": "/static/js/main.1cbdc0e6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
